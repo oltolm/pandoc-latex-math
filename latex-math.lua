721670482a7aa3e70ada2d4840edfe74b27d1418
oltolm <oleg.tolmatcev@gmail.com>
2021-02-19 23:06:31 +0100

    format code

--- latex-math.lua
+++ latex-math.lua
@@ -11,7 +11,7 @@ function appendDepthToSVGFile(depth, svgPath)
     end
 end
 
-function NewLatexRender ()
+function NewLatexRender()
     return {
         preamble = [[
             \usepackage{amsmath}
@@ -21,20 +21,21 @@ function NewLatexRender ()
             \usepackage{colordvi}
             \usepackage[active,tightpage]{preview}
         ]],
-		latexClass =   "article",
-		fontEncoding = "utf8",
-		fontSize =     12,
-		bgcolor =      "#FFFFFF",
-		latexPath =    "latex",
-		dvisvgmPath =  "dvisvgm"
+        latexClass = "article",
+        fontEncoding = "utf8",
+        fontSize = 12,
+        bgcolor = "#FFFFFF",
+        latexPath = "latex",
+        dvisvgmPath = "dvisvgm"
     }
 end
 
-function html2rgb (color)
-	return ''
+function html2rgb(color)
+    return ''
 end
 
 function wrapFormula(lr, latexFormula)
+    local bgcolor = lr.bgcolor ~= "#FFFFFF" and string.format("\\background{%s}\n", html2rgb(lr.bgcolor)) or ''
     local tex = string.format([[\documentclass[%dpt]{%s}
         \usepackage[%s]{inputenc}
         %s
@@ -44,11 +45,7 @@ function wrapFormula(lr, latexFormula)
         %s
         \end{preview}
         \end{document}
-        ]], lr.fontSize, lr.latexClass,
-        lr.fontEncoding,
-        lr.preamble,
-        lr.bgcolor ~= "#FFFFFF" and string.format("\\background{%s}\n", html2rgb(lr.bgcolor)) or '',
-        latexFormula)
+        ]], lr.fontSize, lr.latexClass, lr.fontEncoding, lr.preamble, bgcolor, latexFormula)
     -- io.write(string.format("tex: [[%s]]\n", tex))
     return tex
 end
@@ -58,23 +55,23 @@ function getDepth(out)
     return tonumber(depth)
 end
 
-function renderLatex (lr, latexFormula)
-	local latexDocument = wrapFormula(lr, latexFormula)
-	local currDir = system.get_working_directory()
+function renderLatex(lr, latexFormula)
+    local latexDocument = wrapFormula(lr, latexFormula)
+    local currDir = system.get_working_directory()
     if currDir == nil then
         return nil
     end
     local svgFileName = pandoc.sha1(latexDocument) .. ".svg"
-	local svgPath = currDir .. "/" .. svgFileName
-	local f = io.open(svgPath, "r")
-	if f ~= nil then
-		local depth = getDepth(f:read("a"))
-		f:close()
-		io.write(string.format("found SVG file=%s with depth=%spt\n", svgPath, depth))
-		return depth, svgFileName
-	end
-	-- SVG file does not exist
-	local depth = system.with_temporary_directory("latexmath", function (tmpDir)
+    local svgPath = currDir .. "/" .. svgFileName
+    local f = io.open(svgPath, "r")
+    if f ~= nil then
+        local depth = getDepth(f:read("a"))
+        f:close()
+        io.write(string.format("found SVG file=%s with depth=%spt\n", svgPath, depth))
+        return depth, svgFileName
+    end
+    -- SVG file does not exist
+    local depth = system.with_temporary_directory("latexmath", function(tmpDir)
         system.with_working_directory(tmpDir, function()
             io.write(string.format("changed directory to (%s)\n", tmpDir))
             local tmpFile = io.open("latexmath.tex", "w")
@@ -99,17 +96,17 @@ function renderLatex (lr, latexFormula)
     return depth, svgFileName
 end
 
-function command (lr, svgPath)
-	pandoc.pipe(lr.latexPath, {"--interaction=nonstopmode", "latexmath.tex"}, '')
-	-- out = pandoc.pipe(lr.dvisvgmPath, {"--no-fonts", "-o", svgPath, "latexmath.dvi"}, '')
+function command(lr, svgPath)
+    pandoc.pipe(lr.latexPath, {"--interaction=nonstopmode", "latexmath.tex"}, '')
+    -- out = pandoc.pipe(lr.dvisvgmPath, {"--no-fonts", "-o", svgPath, "latexmath.dvi"}, '')
     local f = io.popen(lr.dvisvgmPath .. " --no-fonts -o \"" .. svgPath .. "\" latexmath.dvi 2>&1")
     local out = f:read("a")
     f:close()
     -- io.write(string.format("out: [[%s]]\n", out))
-	return out
+    return out
 end
 
-function Math (elem)
+function Math(elem)
     local latexFormula1
     local latexFormula = elem.text
     if elem.mathtype == "InlineMath" then
@@ -120,7 +117,9 @@ function Math (elem)
     end
     local lr = NewLatexRender()
     local depth, svgFileName = renderLatex(lr, latexFormula1)
-    local attrs = {alt = latexFormula}
+    local attrs = {
+        alt = latexFormula
+    }
     if depth ~= nil then
         attrs["style"] = string.format("vertical-align:-%spt", depth)
     end
@@ -128,15 +127,17 @@ function Math (elem)
     return pandoc.Image('', svgFileName, '', attrs)
 end
 
-function dump (o)
+function dump(o)
     if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
     else
-       return tostring(o)
+        return tostring(o)
     end
 end
