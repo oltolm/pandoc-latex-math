9ba5cbef49332dd238f9b9289fd648f2089d20ea
oltolm <oleg.tolmatcev@gmail.com>
2021-02-19 23:48:01 +0100

    cleanup

--- latex-math.lua
+++ latex-math.lua
@@ -2,13 +2,8 @@ local system = require("pandoc.system")
 
 function appendDepthToSVGFile(depth, svgPath)
     local f = io.open(svgPath, "a")
-    if f == nil then
-        return nil
-    else
-        f:write(string.format("<!-- depth=%spt -->\n", depth))
-        f:close()
-        return true
-    end
+    f:write(string.format("<!-- depth=%spt -->\n", depth))
+    f:close()
 end
 
 function NewLatexRender()
@@ -58,9 +53,6 @@ end
 function renderLatex(lr, latexFormula)
     local latexDocument = wrapFormula(lr, latexFormula)
     local currDir = system.get_working_directory()
-    if currDir == nil then
-        return nil
-    end
     local svgFileName = pandoc.sha1(latexDocument) .. ".svg"
     local svgPath = currDir .. "/" .. svgFileName
     local f = io.open(svgPath, "r")
@@ -72,24 +64,19 @@ function renderLatex(lr, latexFormula)
     end
     -- SVG file does not exist
     local depth = system.with_temporary_directory("latexmath", function(tmpDir)
-        system.with_working_directory(tmpDir, function()
+        return system.with_working_directory(tmpDir, function()
             io.write(string.format("changed directory to (%s)\n", tmpDir))
             local tmpFile = io.open("latexmath.tex", "w")
             tmpFile:write(latexDocument)
             tmpFile:close()
             local out = command(lr, svgPath)
-            if out == nil then
-                return nil
-            end
             local depth = getDepth(out)
             if depth == nil then
                 io.write(string.format("%s: depth not found\n", svgPath))
-            else
-                io.write(string.format("%s: depth=%spt\n", svgPath, depth))
-                if appendDepthToSVGFile(depth, svgPath) == nil then
-                    return nil
-                end
+                return nil
             end
+            io.write(string.format("%s: depth=%spt\n", svgPath, depth))
+            appendDepthToSVGFile(depth, svgPath)
             return depth
         end)
     end)
@@ -98,7 +85,7 @@ end
 
 function command(lr, svgPath)
     pandoc.pipe(lr.latexPath, {"--interaction=nonstopmode", "latexmath.tex"}, '')
-    -- out = pandoc.pipe(lr.dvisvgmPath, {"--no-fonts", "-o", svgPath, "latexmath.dvi"}, '')
+    -- local out = pandoc.pipe(lr.dvisvgmPath, {"--no-fonts", "-o", svgPath, "latexmath.dvi"}, '')
     local f = io.popen(lr.dvisvgmPath .. " --no-fonts -o \"" .. svgPath .. "\" latexmath.dvi 2>&1")
     local out = f:read("a")
     f:close()
@@ -117,14 +104,14 @@ function Math(elem)
     end
     local lr = NewLatexRender()
     local depth, svgFileName = renderLatex(lr, latexFormula1)
-    local attrs = {
+    local attr = {
         alt = latexFormula
     }
     if depth ~= nil then
-        attrs["style"] = string.format("vertical-align:-%spt", depth)
+        attr["style"] = string.format("vertical-align:-%spt", depth)
     end
-    -- io.write(string.format("%s\n", dump(attrs)))
-    return pandoc.Image('', svgFileName, '', attrs)
+    -- io.write(string.format("%s\n", dump(attr)))
+    return pandoc.Image('', svgFileName, '', attr)
 end
 
 function dump(o)
